54596afd62ac5d6245b1c327660e0e86e49d794e
veli.yetisgengil <yetisgengil_veli@hotmail.com>
2021-11-15 01:19:16 +0300

    Event Bus RabbitMQ Entegre edildi

---
+++ src/BuildingBlocks/EventBus/VY.Ecommerce.EventBus.RabbitMQ/EventBusRabbitMQ.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Polly;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using VY.Ecommerce.EventBus.Base;
+using VY.Ecommerce.EventBus.Base.Events;
+
+namespace VY.Ecommerce.EventBus.RabbitMQ
+{
+    public class EventBusRabbitMQ : BaseEventBus
+    {
+        private ILogger logger;
+        RabbitMQPersistentConnection persistentConnection;
+        private readonly IConnectionFactory connectionFactory;
+        private readonly IModel consumerChannel;
+        public EventBusRabbitMQ(EventBusConfig config, IServiceProvider serviceProvider) : base(config, serviceProvider)
+        {
+            logger = serviceProvider.GetService(typeof(ILogger<EventBusRabbitMQ>)) as ILogger<EventBusRabbitMQ>;
+
+            if(config.Connection != null)
+            {
+                var connJson = JsonConvert.SerializeObject(EventBusConfig.Connection, new JsonSerializerSettings()
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                });
+
+                connectionFactory = JsonConvert.DeserializeObject<ConnectionFactory>(connJson);
+            }
+            else
+            {
+                connectionFactory = new ConnectionFactory();
+            }
+
+            persistentConnection = new RabbitMQPersistentConnection(connectionFactory,config.ConnectionRetryCount);
+            consumerChannel = CreateConsumerChannel();
+
+            SubsManager.OnEventRemoved += SubsManager_OnEventRemoved;
+        }
+
+        private void SubsManager_OnEventRemoved(object sender, string eventName)
+        {
+            eventName = ProcessEventName(eventName);
+
+            if (!persistentConnection.IsConnection)
+            {
+                persistentConnection.TryConnect();
+            }
+
+            consumerChannel.QueueUnbind(queue:eventName,
+                exchange: EventBusConfig.DefaultTopicName,
+                routingKey:eventName);
+
+            if (!SubsManager.IsEmpty)
+            {
+                consumerChannel.Close();
+            }
+        }
+
+        public override void Publish(IntegrationEvent @event)
+        {
+            if (!persistentConnection.IsConnection)
+            {
+                persistentConnection.TryConnect();
+            }
+
+            var policy = Policy.Handle<BrokerUnreachableException>()
+                .Or<SocketException>()
+                .WaitAndRetry(EventBusConfig.ConnectionRetryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), (ex, time) => { 
+                //log
+                
+                });
+
+            var eventName = @event.GetType().Name;
+            eventName = ProcessEventName(eventName);
+
+            consumerChannel.ExchangeDeclare(exchange:EventBusConfig.DefaultTopicName,type:"direct");
+
+            var message = JsonConvert.SerializeObject(@event);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            policy.Execute(() => {
+
+                var properties = consumerChannel.CreateBasicProperties();
+                properties.DeliveryMode = 2;
+
+                consumerChannel.QueueDeclare(queue: GetSubName(eventName),
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+                consumerChannel.BasicPublish(
+                    exchange: EventBusConfig.DefaultTopicName,
+                    routingKey: eventName,
+                    mandatory: true,
+                    basicProperties:properties,
+                    body:body);
+            });
+        }
+
+
+        public override void Subscribe<T, TH>()
+        {
+            var eventName = typeof(T).Name;
+            eventName = ProcessEventName(eventName);
+
+            if (!SubsManager.HasSubscriptionForEvent(eventName))
+            {
+                if (!persistentConnection.IsConnection)
+                {
+                    persistentConnection.TryConnect();
+                }
+
+                consumerChannel.QueueDeclare(queue: GetSubName(eventName), durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+                consumerChannel.QueueBind(queue:GetSubName(eventName),exchange:EventBusConfig.DefaultTopicName,routingKey:eventName);
+            }
+
+            logger.LogInformation("Subscription to event {EventName} with {EventHandler}", eventName, typeof(TH).Name);
+
+            SubsManager.AddSubscription<T, TH>();
+            StartBasicConsume(eventName);
+        }
+
+        public override void UnSubscribe<T, TH>()
+        {
+            SubsManager.RemoveSubscription<T, TH>();
+        }
+
+        private IModel CreateConsumerChannel()
+        {
+            if (!persistentConnection.IsConnection)
+            {
+                persistentConnection.TryConnect();
+            }
+            var channel = persistentConnection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: EventBusConfig.DefaultTopicName, type: "direct");
+            return channel;
+        }
+        private void StartBasicConsume(string eventName)
+        {
+            if(consumerChannel != null)
+            {
+                var consumer = new EventingBasicConsumer(consumerChannel);
+                consumer.Received += Consumer_Received;
+                consumerChannel.BasicConsume(queue:GetSubName(eventName),autoAck:false,consumer:consumer);
+
+            }
+        }
+
+        private async void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var eventName = e.RoutingKey;
+            eventName = ProcessEventName(eventName);
+            var message = Encoding.UTF8.GetString(e.Body.Span);
+
+            try
+            {
+                await ProcessEvent(eventName,message);
+            }
+            catch (Exception ex)
+            {
+                //login
+            }
+            consumerChannel.BasicAck(e.DeliveryTag,multiple:false);
+        }
+    }
+}
---
+++ src/BuildingBlocks/EventBus/VY.Ecommerce.EventBus.RabbitMQ/RabbitMQPersistentConnection.cs
@@ -0,0 +1,86 @@
+﻿using Polly;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VY.Ecommerce.EventBus.RabbitMQ
+{
+    public class RabbitMQPersistentConnection : IDisposable
+    {
+        private readonly IConnectionFactory connectionFactory;
+        private readonly int retryCount;
+        private IConnection connection;
+        private object lock_object = new object();
+        private bool _dispose;
+
+        public RabbitMQPersistentConnection(IConnectionFactory connectionFactory,int retryCount=5)
+        {
+            this.connectionFactory = connectionFactory;
+            this.retryCount = retryCount;
+        }
+        public bool IsConnection => connection != null && connection.IsOpen;
+
+        public IModel CreateModel()
+        {
+            return connection.CreateModel();
+        }
+
+        public void Dispose()
+        {
+            _dispose = true;
+            connection.Dispose();
+        }
+
+        public bool TryConnect()
+        {
+            lock (lock_object)
+            {
+                var policy = Policy.Handle<SocketException>()
+                    .Or<BrokerUnreachableException>()
+                    .WaitAndRetry(retryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), (ex, time) =>
+                         {
+
+                         }
+                         );
+                policy.Execute(()=> { connection = connectionFactory.CreateConnection(); });
+
+                if (IsConnection)
+                {
+                    //log
+                    connection.ConnectionShutdown += Connection_ConnectionShutdown;
+                    connection.ConnectionBlocked += Connection_ConnectionBlocked; 
+                    connection.CallbackException += Connection_CallbackException;
+                    return true;
+                }
+                return false;
+
+            }
+        }
+
+        private void Connection_CallbackException(object sender, global::RabbitMQ.Client.Events.CallbackExceptionEventArgs e)
+        {
+            if (_dispose) return;
+            //log
+            TryConnect();
+        }
+
+        private void Connection_ConnectionBlocked(object sender, global::RabbitMQ.Client.Events.ConnectionBlockedEventArgs e)
+        {
+            if (_dispose) return;
+            //log
+            TryConnect();
+        }
+
+        private void Connection_ConnectionShutdown(object sender, ShutdownEventArgs e)
+        {
+            if (_dispose) return;
+            //log
+            TryConnect();
+        }
+    }
+}
