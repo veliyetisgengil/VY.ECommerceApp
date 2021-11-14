f0c46db44bfcce574c110d400028c797789849c8
veli.yetisgengil <yetisgengil_veli@hotmail.com>
2021-11-15 01:25:58 +0300

    Eventbus factory tamamlandı.

---
+++ src/BuildingBlocks/EventBus/VY.Ecommerce.EventBus.Factory/EventBusFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VY.Ecommerce.EventBus.AzureServiceBus;
+using VY.Ecommerce.EventBus.Base;
+using VY.Ecommerce.EventBus.Base.Abstraction;
+using VY.Ecommerce.EventBus.RabbitMQ;
+
+namespace VY.Ecommerce.EventBus.Factory
+{
+    public static class EventBusFactory
+    {
+        public static IEventBus Create(EventBusConfig config,IServiceProvider serviceProvider)
+        {
+            return config.EventBusType switch
+            {
+                EventBusType.AzureServiceBus => new EventBusServiceBus(config,serviceProvider),
+                _ => new EventBusRabbitMQ(config,serviceProvider),
+            };
+        }
+
+    }
+}
